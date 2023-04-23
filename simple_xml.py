4f3d74e62374f5ee95d695a0642fe8bf3db0f671
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2023-04-23 18:03:41 +0300

    Improve xml

--- simple_xml.py
+++ simple_xml.py
@@ -3,6 +3,71 @@ import struct
 import json
 import xml.etree.ElementTree as ET
 
+DATA_TYPES = {
+    0: "string",
+    1: "integer",
+    3: "uuid",
+    4: "byte",
+    5: "buffer",
+}
+
+
+# We could read these dynamically. But I just don't
+COLUMN_NAMES = [
+    "Text",
+    "Group",
+    "Define",
+    "Name",
+    "CardId",
+    "Info",
+    "Mesh",
+    "Class1",
+    "Class2",
+    "Level0",
+    "Level1",
+    "Level2",
+    "Level3",
+    "Level4",
+    "Level5",
+    "Level6",
+    "Level7",
+    "Level8",
+    "Level9",
+    "unknown",
+    "SpellDesc",
+    "PriceA",
+    "PriceB",
+    "PriceC",
+    "Script1",
+    "Script2",
+    "Script3",
+    "Script4",
+    "Script5",
+    "Npc",
+    "Voice",
+    "Special",
+    "Script",
+    "MHP",
+    "EvolCId",
+    "EvolVar",
+    "MovSpeed",
+    "Class0",
+    "Mana",
+    "Loadup",
+    "Trajectory",
+    "MissileEffect",
+    "ImpactEffect",
+    "Damage",
+    "Type",
+    "Behaviour",
+    "JumpPower",
+    "CriticalHit",
+    "Glow",
+    "Sphere",
+    "LevelUp",
+]
+
+
 class Byte:
     def __init__(self, f):
         self.value = struct.unpack("<B", f.read(1))[0]
@@ -78,10 +143,17 @@ class ColumnData:
                 assert Uint(f).value == 1
                 return Byte(f)
             case 5:
-                return Buffer(f)
+                raise Exception("Buffers exist in documentation, but not ingame. We don't support them")
+            #     return Buffer(f)
 
     def xml(self):
-        element = ET.Element("ColumnData", attrib={"type": f"{self.datatype.value}", "index": f"{self.index.value}"})
+        element = ET.Element(
+            "ColumnData",
+            attrib = {
+                "type": f"{DATA_TYPES[self.datatype.value]}",
+                "index": f"{COLUMN_NAMES[self.index.value]}",
+            }
+        )
         element.append(self.item.xml())
         return element
 
@@ -92,20 +164,20 @@ class Uuid:
         self.type = Uint(f)
 
     def xml(self):
-        element = ET.Element("Uuid", attrib={"uid": f"{self.uid.value}"})
-        element.append(self.type.xml())
+        element = ET.Element("Uuid", attrib={"type": f"{self.type.value}"})
+        element.text = f"{self.uid.value}"
         return element
 
 
-class Buffer:
-    def __init__(self, f):
-        self.value = [Byte(f) for index in range(Uint(f).value)]
-
-    def xml(self):
-        element = ET.Element("Buffer")
-        for item in self.value:
-            element.append(item.xml())
-        return element
+# class Buffer:
+#     def __init__(self, f):
+#         self.value = [Byte(f) for index in range(Uint(f).value)]
+#
+#     def xml(self):
+#         element = ET.Element("Buffer")
+#         for item in self.value:
+#             element.append(item.xml())
+#         return element
 
 
 class IndexTable:
