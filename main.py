cb4cee060401429ae965447411a8d1d2595b73ba
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2024-03-11 19:32:51 +0300

    Start work on toml export

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import xml.etree.ElementTree as ET
 
-from src.nodes.indextable import IndexTable
-from src.nodes.table import Table
+import tomli_w
+
+from src.db.nodes.indextable import IndexTable
+from src.db.nodes.table import Table
 
 
 def read_fbs(path: Path) -> IndexTable | Table:
@@ -32,6 +34,29 @@ if __name__ == "__main__":
     for filepath in DATA_PATH.glob("*.fbs"):
         table_id = int(filepath.stem[-1])
         tables[table_id] = read_fbs(filepath)
+
+    # for row in tables[6].value:
+    #     for cell in row.cells:
+    #         if cell.datatype.value == 0:
+    #             cell.item.value = cell.item.value.upper()
+
+
+    (BUILD_PATH / "scripts").mkdir(exist_ok=True)
+    for row in tables[5].value:
+
+        script = {
+            "uid": hex(row.uid.value).upper()[2:],
+            "name": hex(row.cells[0].item.uid.value).upper()[2:],
+            "Script1": row.cells[1].item.value[:-1],
+            "Script2": row.cells[2].item.value[:-1],
+            "Script3": row.cells[3].item.value[:-1],
+            "Script4": row.cells[4].item.value[:-1],
+            "Script5": row.cells[5].item.value[:-1],
+        }
+        filename = row.cells[-1].item.value.replace('\0', '')
+        with open((BUILD_PATH / "scripts" / f"{filename}.toml"), "wb") as f:
+            tomli_w.dump(script, f, multiline_strings=True)
+
 
     for table_id, table in tables.items():
         write_xml(table, BUILD_PATH / f"_fb0x0{table_id}.xml")
